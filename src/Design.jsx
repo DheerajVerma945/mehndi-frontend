2069a4f264f146c6b42fe14865da62371ed45ddc
Dheeraj Verma <vermadheeraj945@gmail.com>
2025-05-14 18:03:43 +0530

    Made a change in Ci branch

--- src/Design.jsx
+++ src/Design.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import {axiosInstance} from "./utils/axiosInstance";
 import {
-  MapPinIcon,
   ClockIcon,
   FileTextIcon,
   MessageCircle,
